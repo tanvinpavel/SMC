fef7ef4845789a2fffef33159a6b768f019ada68
Rakib Hossain <rakibjp431@gmail.com>
2022-09-03 20:41:36 +0600

    add our api page

---
+++ src/Components/Pages/OurApi/ApiCard.jsx
@@ -0,0 +1,142 @@
+import React from 'react'
+
+function ApiCard() {
+  return (
+    <>
+        <div class="container">
+          <div className="cards">
+           <div className="card-title ">
+               <h2>Work with SMC API’s  </h2>
+                <p>SMC has built API’s in 10 programming languages to make it easy for anyone to quickly implement SMC in their applications and services.  </p>
+           </div>
+
+           <div className="container">
+
+            <div class=" row  row-cols-sm-2 row-cols-md-3 row-cols-lg-5">
+                <div class="card cardapi col">
+                    <img src="image/api/node.png" class="card-img-top" alt="..."/>
+                    <div class="card-body api-card-body">
+                    <h5 class="card-title api-card-title">Node JS </h5>
+                    <p class="card-text api-card-text">Start Building an SMC App Using our Node JS API’s. </p>
+                    <h6 className='api-version'>Version 1.035</h6>
+                    <div>
+                        <a href="#" class="btn-fill api-btn">click me</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="card cardapi col">
+                    <img src="image/api/node.png" class="card-img-top" alt="..."/>
+                    <div class="card-body api-card-body">
+                    <h5 class="card-title api-card-title">Node JS </h5>
+                    <p class="card-text api-card-text">Start Building an SMC App Using our Node JS API’s. </p>
+                    <h6 className='api-version'>Version 1.035</h6>
+                    <div>
+                        <a href="#" class="btn-fill api-btn">click me</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="card cardapi col">
+                    <img src="image/api/node.png" class="card-img-top" alt="..."/>
+                    <div class="card-body api-card-body">
+                    <h5 class="card-title api-card-title">Node JS </h5>
+                    <p class="card-text api-card-text">Start Building an SMC App Using our Node JS API’s. </p>
+                    <h6 className='api-version'>Version 1.035</h6>
+                    <div>
+                        <a href="#" class="btn-fill api-btn">click me</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="card cardapi col">
+                    <img src="image/api/node.png" class="card-img-top" alt="..."/>
+                    <div class="card-body api-card-body">
+                    <h5 class="card-title api-card-title">Node JS </h5>
+                    <p class="card-text api-card-text">Start Building an SMC App Using our Node JS API’s. </p>
+                    <h6 className='api-version'>Version 1.035</h6>
+                    <div>
+                        <a href="#" class="btn-fill api-btn">click me</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="card cardapi col">
+                    <img src="image/api/node.png" class="card-img-top" alt="..."/>
+                    <div class="card-body api-card-body">
+                    <h5 class="card-title api-card-title">Node JS </h5>
+                    <p class="card-text api-card-text">Start Building an SMC App Using our Node JS API’s. </p>
+                    <h6 className='api-version'>Version 1.035</h6>
+                    <div>
+                        <a href="#" class="btn-fill api-btn">click me</a>
+                        </div>
+                    </div>
+                </div>
+
+            
+           <div class="card cardapi">
+               <img src="image/api/node.png" class="card-img-top" alt="..."/>
+               <div class="card-body api-card-body">
+               <h5 class="card-title api-card-title">Node JS </h5>
+               <p class="card-text api-card-text">Start Building an SMC App Using our Node JS API’s.  </p>
+               <h6 className='api-version'>Version 1.035</h6>
+                  <div>
+                   <a href="#" class="btn-fill api-btn">click me</a>
+                   </div>
+               </div>
+           </div>
+           <div class="card cardapi">
+               <img src="image/api/node.png" class="card-img-top" alt="..."/>
+               <div class="card-body api-card-body">
+               <h5 class="card-title api-card-title">Node JS </h5>
+               <p class="card-text api-card-text">Start Building an SMC App Using our Node JS API’s. </p>
+               <h6 className='api-version'>Version 1.035</h6>
+                  <div>
+                   <a href="#" class="btn-fill api-btn">click me</a>
+                   </div>
+               </div>
+           </div>
+
+           <div class="card cardapi">
+               <img src="image/api/node.png" class="card-img-top" alt="..."/>
+               <div class="card-body api-card-body">
+               <h5 class="card-title api-card-title">Node JS </h5>
+               <p class="card-text api-card-text">Start Building an SMC App Using our Node JS API’s. </p>
+               <h6 className='api-version'>Version 1.035</h6>
+                  <div>
+                   <a href="#" class="btn-fill api-btn">click me</a>
+                   </div>
+               </div>
+           </div>
+
+           <div class="card cardapi">
+               <img src="image/api/node.png" class="card-img-top" alt="..."/>
+               <div class="card-body api-card-body">
+               <h5 class="card-title api-card-title">Node JS </h5>
+               <p class="card-text api-card-text">Start Building an SMC App Using our Node JS API’s.  </p>
+               <h6 className='api-version'>Version 1.035</h6>
+                  <div>
+                   <a href="#" class="btn-fill api-btn">click me</a>
+                   </div>
+               </div>
+           </div>
+
+           <div class="card cardapi">
+               <img src="image/api/node.png" class="card-img-top" alt="..."/>
+               <div class="card-body api-card-body">
+               <h5 class="card-title api-card-title">Node JS </h5>
+               <p class="card-text api-card-text">Start Building an SMC App Using our Node JS API’s. </p>
+               <h6 className='api-version'>Version 1.035</h6>
+                  <div>
+                   <a href="#" class="btn-fill api-btn ">click me</a>
+                   </div>
+               </div>
+           </div>
+                </div>
+           </div>
+   
+          </div>
+        </div>
+    </>
+  )
+}
+
+export default ApiCard

5260d8885c03368dc4c0917c1af0d43fe2e0c114
tanvinpavel <tanvinpavel123@gmail.com>
2022-09-05 14:08:47 +0600

    Video Modal Add

--- src/Components/Pages/SetupEnvironment/Instructions/Instructions.jsx
+++ src/Components/Pages/SetupEnvironment/Instructions/Instructions.jsx
@@ -5,7 +5,7 @@ const Instructions = () => {
         <div className='instructions py-5'>
             <div className="container">
                 <div className="row">
-                    <div className="col-md-12 col-lg-5 mt-5 mt-lg-0 instructions-text order-2">
+                    <div className="col-md-12 col-lg-5 mt-5 mt-lg-0 instructions-text order-2 order-lg-1">
                         <h4>Step by Step Instructions</h4>
                         <p>We recommend starting with a framework to speed up development, especially if you’re just starting out. Below you can find step by step information to get started with SMC.</p>
                         <ul>
@@ -16,7 +16,7 @@ const Instructions = () => {
                             <li>Step 5 - Download XYZ Library, Run XYZ Plugin</li>
                         </ul>
                     </div>
-                    <div className="col-md-12 col-lg-7 order-1">
+                    <div className="col-md-12 col-lg-7 order-1 order-lg-2">
                         <img src="./image/API's/instruction.png" alt="" />
                     </div>
                 </div>
